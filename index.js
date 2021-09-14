4628c695f0f3fbabdcd57a55646d6c43e04a16de
dan <dan@localhost.localdomain>
2021-09-14 14:23:03 -0400

    updating for Prod

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-import express from 'express';
-import userRoutes from './src/routes/userRoutes';
-import serviceCategoryRoutes from './src/routes/serviceCategoryRoutes';
-import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
-import newsRoutes from './src/routes/newsRoutes';
-import estimatesRoute from './src/routes/estimatesRoutes';
+import express from "express";
+import userRoutes from "./src/routes/userRoutes";
+import serviceCategoryRoutes from "./src/routes/serviceCategoryRoutes";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import newsRoutes from "./src/routes/newsRoutes";
+import estimatesRoute from "./src/routes/estimatesRoutes";
 
-const app = express()
+const app = express();
 const PORT = 4000;
 
 //mongoose connection
 mongoose.Promise = global.Promise;
 
 // dev
-mongoose.connect('mongodb+srv://admin:rootadmin@cluster0.wt8ns.mongodb.net/blueskydb', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-//prod
-// mongoose.connect('mongodb+srv://admin:rootadmin@cluster0.wt8ns.mongodb.net/blueSkydb?retryWrites=true&w=majority', {
+// mongoose.connect('mongodb+srv://admin:rootadmin@cluster0.wt8ns.mongodb.net/blueskydb', {
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true
 // });
 
+//prod
+mongoose.connect(
+  "mongodb+srv://admin:rootadmin@cluster0.wt8ns.mongodb.net/blueSkydb?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
 // cors setup
-const cors = require('cors');
-app.use(cors({
-    origin: 'http://localhost:3000',
-}));
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 //bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -40,10 +45,8 @@ serviceCategoryRoutes(app);
 newsRoutes(app);
 estimatesRoute(app);
 
-app.get('/', (req, res) =>
-    res.send (`Node and express server running on port ${PORT}`)
+app.get("/", (req, res) =>
+  res.send(`Node and express server running on port ${PORT}`)
 );
 
-app.listen(PORT, () => 
-    console.log(`Your server is running on port ${PORT}`)
-);
+app.listen(PORT, () => console.log(`Your server is running on port ${PORT}`));
